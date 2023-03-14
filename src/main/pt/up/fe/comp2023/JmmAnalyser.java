fcd01b36052587cd3748a90703d521236150f3e7
Vasco Guedes <up202004395@g.uporto.pt>
2023-03-14 17:37:12 +0000

    commented code + JmmAnalyser return fix

--- src/main/pt/up/fe/comp2023/JmmAnalyser.java
+++ src/main/pt/up/fe/comp2023/JmmAnalyser.java
@@ -2,6 +2,7 @@ package pt.up.fe.comp2023;
 
 import pt.up.fe.comp.jmm.analysis.JmmAnalysis;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.parser.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
 
@@ -11,12 +12,14 @@ public class JmmAnalyser implements JmmAnalysis {
 
     @Override
     public JmmSemanticsResult semanticAnalysis(JmmParserResult jmmParserResult) {
+        JmmNode node = jmmParserResult.getRootNode();
+
         JmmSymbolTable symbolTable=new JmmSymbolTable();
         List<Report> reports = new ArrayList<>();
 
-        SymbolTableVisitor visitor= new SymbolTableVisitor(symbolTable,reports);
-        visitor.visit(jmmParserResult.getRootNode(),"");
+        SymbolTableVisitor visitor= new SymbolTableVisitor(symbolTable, reports);
+        visitor.visit(node,"");
 
-        return null;
+        return new JmmSemanticsResult(jmmParserResult, symbolTable, reports);
     }
 }
