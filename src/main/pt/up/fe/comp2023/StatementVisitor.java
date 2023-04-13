070d176e8ea02ed84287da687f6601bd511c13a3
brunabrasil <up202007191@g.uporto.pt>
2023-04-13 16:14:54 +0100

    Array assignment done

--- src/main/pt/up/fe/comp2023/StatementVisitor.java
+++ src/main/pt/up/fe/comp2023/StatementVisitor.java
@@ -34,6 +34,76 @@ public class StatementVisitor extends AJmmVisitor<String, Type> {
     }
 
     private Type dealWithAssignmentArray(JmmNode jmmNode, String s) {
+        String varLeft = jmmNode.get("var");
+
+        ExpressionVisitor expressionVisitor = new ExpressionVisitor(table, reports);
+        Type indexType = expressionVisitor.visit(jmmNode.getJmmChild(0), "");
+        Type rightType = expressionVisitor.visit(jmmNode.getJmmChild(1), "");
+
+        JmmNode parent = jmmNode.getJmmParent();
+        Type leftType= new Type("", false);
+
+        while(!parent.getKind().equals("NormalMethod") && !parent.getKind().equals("MainMethod")) {
+            parent = parent.getJmmParent();
+        }
+        //see if var is a field
+        List<Symbol> fields = table.getFields();
+        if(fields != null){
+            for (int i = 0; i < fields.size(); i++){
+                if(fields.get(i).getName().equals(varLeft)){
+                    leftType = fields.get(i).getType();
+                    break;
+                }
+            }
+        }
+
+        //see if var is a parameter
+        List<Symbol> parameters;
+        if(parent.getKind().equals("NormalMethod")){
+            parameters = table.getParameters(parent.get("methodName"));
+        }
+        else{
+            parameters = table.getParameters("main");
+        }
+        if(parameters != null){
+            for (int i = 0; i < parameters.size(); i++){
+                if(parameters.get(i).getName().equals(varLeft)){
+                    leftType = parameters.get(i).getType();
+                    break;
+                }
+            }
+        }
+
+        //see if var is a local variable
+        List<Symbol> localVariables;
+        if(parent.getKind().equals("NormalMethod")){
+            localVariables = table.getLocalVariables(parent.get("methodName"));
+        }
+        else{
+            localVariables = table.getLocalVariables("main");
+        }
+        if(localVariables != null){
+            for (int i = 0; i < localVariables.size(); i++){
+                if(localVariables.get(i).getName().equals(varLeft)){
+                    leftType = localVariables.get(i).getType();
+                    break;
+                }
+            }
+        }
+        int line = Integer.valueOf(jmmNode.get("lineStart"));
+        int col = Integer.valueOf(jmmNode.get("colStart"));
+
+        if(!leftType.isArray()){
+            reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, line, col, "Var is not an array"));
+        }
+        if(!indexType.getName().equals("int")){
+            reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, line, col, "Index must be of type int"));
+        }
+        if(!rightType.getName().equals("int")){
+            reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, line, col, "Assigned must be of type int"));
+        }
+
+
         return null;
     }
 
